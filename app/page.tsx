c89ef64cab728037524376cfc687c3e91975c901
arvinduh <arvin.yu.duh@gmail.com>
2026-04-02 15:53:18 -0700

    hydration bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,18 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { LandingPage } from "./components/LandingPage/LandingPage";
-import { OwnerHome } from "./components/OwnerHome/OwnerHome";
-import { loadDogProfile } from "@/lib/dog-profile";
-import type { DogProfile } from "@/app/create-dog/types";
+import type { DogProfile } from '@/app/create-dog/types';
+import { loadDogProfile } from '@/lib/dog-profile';
+import { useLayoutEffect, useState } from 'react';
+import { LandingPage } from './components/LandingPage/LandingPage';
+import { OwnerHome } from './components/OwnerHome/OwnerHome';
 
 export default function Home() {
-  const [dog] = useState<DogProfile | null>(() =>
-    typeof window !== "undefined" ? loadDogProfile() : null,
-  );
+  const [dog, setDog] = useState<DogProfile | null>(null);
+
+  useLayoutEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    setDog(loadDogProfile());
+  }, []);
 
   if (!dog) {
     return <LandingPage />;
